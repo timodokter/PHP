022ccc9dacbbedf0765c1ee7018af0dbd79e017e
Timo Dokter <timo.dokter@student.rocvf.nl>
2020-12-01 11:46:44 +0100

    opdr 3.2 is af

--- hst3/opdr3.2/index.php
+++ hst3/opdr3.2/index.php
@@ -1,1 +1,14 @@
+<head>
+<title>kerstboom</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
 <?php
+for ($x = 0; $x <=9; $x++) {
+    for ($i = 0; $i < $x; $i++) {
+        echo "*";
+    }
+    echo "*<br>";
+}
+?>
+</body>
