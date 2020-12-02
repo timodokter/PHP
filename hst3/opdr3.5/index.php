ef930b9bc2668a5b3d543afb226dc74d1593fb08
Timo Dokter <timo.dokter@student.rocvf.nl>
2020-12-02 09:54:52 +0100

    opdr 3.5 is af

--- hst3/opdr3.5/index.php
+++ hst3/opdr3.5/index.php
@@ -1,10 +1,20 @@
 <html lang="en">
 <head>
-    <title></title>
+    <title>opdr3.5</title>
 </head>
 <body>
 <?php
-
+$leeftijd = 12;
+$prijs = 10.00;
+if ($leeftijd > 65 OR $leeftijd <= 12 And $leeftijd > 3) {
+    $prijs = $prijs / 2;
+    echo "U bent " . $leeftijd . " jaar oud en daarom betaalt u €" . $prijs;
+} else if ($leeftijd <= 2) {
+    $prijs = $prijs == 0;
+    echo "U bent " . $leeftijd . " jaar oud en daarom bent u gratis!";
+} else {
+    echo "U bent " . $leeftijd . " jaar oud en daarom betaalt u €" . $prijs;
+}
 ?>
 </body>
 </html>
