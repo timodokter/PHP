237e1cd544ff5a1fde61bd416c9cc20ec3db9835
Timo Dokter <timo.dokter@student.rocvf.nl>
2020-12-02 15:00:36 +0100

    opdr 3.7

---
+++ hst3/opdr3.7/index.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <title>opdr 3.7</title>
+</head>
+<body>
+<?php
+$afspraak = "";
+$tijd = "";
+$kappersagenda = array($tijd = "9.15"=> $afspraak = "Mevr. Pietersen", $tijd = "9.30"=> $afspraak = "Mevr. Willems", $tijd = "9.45"=> $afspraak = "", $tijd = "10.00"=> $afspraak = "Paul van den Broek", $tijd = "10.15"=> $afspraak = "Karel de Meeuw", $tijd = "10.30"=> $afspraak = "");
+print("De volgende momenten zijn nog beschikbaar:<ul>");
+foreach ($kappersagenda as $tijd => $afspraak) {
+    if($afspraak == "") {
+        print("<li>".$tijd."</li>");
+    }
+}
+print("</ul>");
+?>
+</body>
+</html>
