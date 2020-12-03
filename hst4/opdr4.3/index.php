7e5fdb8a95d161e87c9f27f61b86f7d70d9a6ec1
Timo Dokter <timo.dokter@student.rocvf.nl>
2020-12-03 10:15:56 +0100

    opdr 4.3 is af

--- hst4/opdr4.3/index.php
+++ hst4/opdr4.3/index.php
@@ -1,10 +1,22 @@
 <html lang="en">
 <head>
     <title></title>
+    <style>
+        html {
+            font-family: "Arial", sans-serif;
+        }
+    </style>
 </head>
 <body>
 <?php
-
+echo omdraaier('hoi');
+    function omdraaier($input) {
+        if (is_string($input) == 1) {
+            return strrev($input);
+        } else {
+            echo 'Uw input is geen tekst!';
+        }
+    }
 ?>
 </body>
 </html>
