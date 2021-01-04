4b9183e4fbe375b3f41208a71f1f4cd903069757
Timo Dokter <timo.dokter@student.rocvf.nl>
2021-01-04 14:27:59 +0100

    opdr 8.4 is af

--- hst8/opdr4/radio.php
+++ hst8/opdr4/radio.php
@@ -2,19 +2,28 @@
 class Radioprogramma {
     private $ProgrammaNaam;
     private $Omschrijving;
-    private $Liedjes = array();
+    private $liedjes;
 
-    public function getProgramma($IngevuldeProgrammaNaam,
-                                 $IngevuldeOmschrijving) {
-        $this->ProgrammaNaam = $IngevuldeProgrammaNaam;
-        $this->Omschrijving = $IngevuldeOmschrijving;
+    public function setProgramma() {
+        $this->ProgrammaNaam ='<h1>The Search</h1>';
+        $this->Omschrijving = '<h3>Dit is het nieuwste album van nf</h3>';
+        echo $this->ProgrammaNaam . '<br>';
+        echo $this->Omschrijving . '<br>';
     }
 
-    public function getLiedjes($IngevuldeLiedjes = array()) {
-        $this->Liedjes = $IngevuldeLiedjes;
-        echo $this->Liedjes;
+    public function setLiedjes ($ingevuldeLiedjes){
+        //korte if (werkt alleen als je 1 statement na de if en geen else)
+        if (is_array($ingevuldeLiedjes)) $this->liedjes = $ingevuldeLiedjes;
+    }
+
+    public function getLiedjes() {
+       foreach ($this->liedjes as $value){
+           echo $value . "<br>";
+       }
     }
 
 }
 $ProgrammaInfo = new Radioprogramma();
+$ProgrammaInfo->setProgramma();
+$ProgrammaInfo->setLiedjes(array("The Search", "Leave Me Alone", 'Change', 'My Stress', 'Nate'));
 $ProgrammaInfo->getLiedjes();
