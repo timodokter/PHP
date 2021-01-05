35b9a53f4c0869f78f006275853f48499f1a717f
Timo Dokter <timo.dokter@student.rocvf.nl>
2021-01-05 09:45:06 +0100

    ik ben begonnen aan 8.6. het form is aangemaakt

---
+++ hst8/opdr6/mrWheely.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <title>MR Wheely</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+<div id="wrapper">
+<img class="header" src="img/wheelyHeader.jpg" alt="header-img">
+    <form action="mrWheely.php" method="post">
+
+<?php
+
+?>
+</body>
+</html>
