ec1167dbe744d0fdc0b804587930d2f13d91e06a
Timo Dokter <timo.dokter@student.rocvf.nl>
2021-01-05 09:23:18 +0100

    opdr 8.5 is af

---
+++ hst8/opdr5/apen.php
@@ -0,0 +1,34 @@
+<html lang="en">
+<head>
+    <title>Apen kiezer</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+<div id="wrapper">
+    <?php
+    echo '<img src="img/monkey-business.jpg"> <br>';
+    echo '<h2>Select your monkey!</h2>';
+    echo '<img src="img/monkey_swings.png">';
+    echo '<div id="linkjes">';
+    echo "<a class='aap' href='https://www.google.nl/search?q=Baviaan&tbm=isch'>Baviaan</a><br>";
+    echo "<a class='aap' href='https://www.google.nl/search?q=Guereza&tbm=isch'>Guereza</a><br>";
+    echo "<a class='aap' href='https://www.google.nl/search?q=Langoer&tbm=isch'>Langoer</a><br>";
+    echo "<a class='aap' href='https://www.google.nl/search?q=Neusaap&tbm=isch'>Neusaap</a><br>";
+    echo "<a class='aap' href='https://www.google.nl/search?q=Tamarin&tbm=isch'>Tamarin</a><br>";
+    echo "<a class='aap' href='https://www.google.nl/search?q=Brulaap&tbm=isch'>Brulaap</a><br>";
+    echo "<a class='aap' href='https://www.google.nl/search?q=Halfaap&tbm=isch'>Halfaap</a><br>";
+    echo "<a class='aap' href='https://www.google.nl/search?q=Mandril&tbm=isch'>Mandril</a><br>";
+    echo "<a class='aap' href='https://www.google.nl/search?q=Oeakari&tbm=isch'>Oeakari</a><br>";
+    echo "<a class='aap' href='https://www.google.nl/search?q=Faunaap&tbm=isch'>Faunaap</a><br>";
+    echo "<a class='aap' href='https://www.google.nl/search?q=Hoelman&tbm=isch'>Hoelman</a><br>";
+    echo "<a class='aap' href='https://www.google.nl/search?q=Meerkat&tbm=isch'>Meerkat</a><br>";
+    echo "<a class='aap' href='https://www.google.nl/search?q=Oormaki&tbm=isch'>Oormaki</a><br>";
+    echo "<a class='aap' href='https://www.google.nl/search?q=Gorilla&tbm=isch'>Gorilla</a><br>";
+    echo "<a class='aap' href='https://www.google.nl/search?q=Kuifaap&tbm=isch'>Kuifaap</a><br>";
+    echo "<a class='aap' href='https://www.google.nl/search?q=Mensaap&tbm=isch'>Mensaap</a><br>";
+    echo "<a class='aap' href='https://www.google.nl/search?q=Spinaap&tbm=isch'>Spinaap</a><br>";
+    echo '</div>';
+    ?>
+</div>
+</body>
+</html>
